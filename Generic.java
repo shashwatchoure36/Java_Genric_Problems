b31367cb4e0c7806810bf49b854b1759fbb10c7d
shashwatchoure365 <shashwatchoure36@gmail.com>
2023-04-23 14:21:03 +0530

    [Shashwat] Add : This is java program to fined max Integer

---
+++ Generic.java
@@ -0,0 +1,42 @@
+public class Generic_Find_Max {
+	
+	public static void toPrintMaxInteger(Integer[] inputArray) {
+		System.out.println("ELEMENTS IN ARRAY : ");
+		for (int element : inputArray) {
+			System.out.printf("%s", element);
+			System.out.println();
+		}
+		System.out.println();
+
+		int x;
+		int max = inputArray[0];
+		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
+		for (int i = 1; i < inputArray.length; i++) {
+			/*
+			 * Using compareTo method 
+			 * to find maximum among 
+			 * integer values in array
+			 */
+			if ((inputArray[i].compareTo(max)) > 0) {
+				max = inputArray[i];
+			}
+		}
+		System.out.println(max);
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println();
+		System.out.println("WELCOME TO FIND MAXIMUM PROBLEM USING GENERICS PROGRAM");
+		System.out.println("---------------------------------------------------------");
+
+		// Integer array
+		Integer[] intarr = { 10, 35, 41, 2, 6 };
+
+		Generic_Find_Max.toPrintMaxInteger(intarr);	
+
+	}
+
+}
